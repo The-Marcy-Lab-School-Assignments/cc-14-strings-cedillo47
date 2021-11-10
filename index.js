e441b6ab59963ca6ec2ab9d4a65cee59480e165d
leo <cedillo47@gmail.com>
2021-11-10 15:22:05 +0000

    done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 //write your code here
+
+const stripUpperCase = string => {
+  let lowerCase = "";
+  for (let i = 0; i < string.length; i++){
+    if (string[i] === string[i].toLowerCase()){
+      lowerCase += string[i]
+    }
+  } return lowerCase; 
+};
+
+console.log(stripUpperCase("Hello!"))
+console.log(stripUpperCase("SevenEleven"))
+console.log(stripUpperCase("Don't play with Me!"))
